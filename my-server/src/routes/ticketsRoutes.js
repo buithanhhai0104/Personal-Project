7a01b3fabb1266c6187339360d2111bb30fe3332
thanhhai <buithanhhai0499@gmail.com>
2025-02-17 21:33:21 +0700

    commit

--- my-server/src/routes/ticketsRoutes.js
+++ my-server/src/routes/ticketsRoutes.js
@@ -2,11 +2,7 @@ const express = require("express");
 const router = express.Router();
 const ticketsController = require("../controllers/ticketsController");
 const authController = require("../controllers/authController");
-router.post(
-  "/book-ticket",
-  authController.verifyToken,
-  ticketsController.bookTicket
-);
+router.post("/book-ticket", ticketsController.bookTicket);
 router.get("/:ticket_id", ticketsController.getTicketByTicketId);
 router.put("/status", ticketsController.updateMultipleTicketStatus);
 router.get("/", ticketsController.getAllTickets);
